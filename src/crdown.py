b8c189cde582116fab59050fb784d47cb27edd94
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-08-07 14:48:33 -0300

    Cosmetic fixes

--- src/crdown.py
+++ src/crdown.py
@@ -8,17 +8,22 @@ from crunchy import login
 
 __version__ = '0.1'
 
+
 def argparser():
     parser = argparse.ArgumentParser(
-            description='Crunchyroll video downloader')
-    parser.add_argument(dest='url', nargs='?', action='store', metavar='URL',
-    		help='Video url from Crunchyroll to download')
-    parser.add_argument('-l', '--login', action='store_true', default=0,
-            help='Login to your Crunchyroll account')
+        description='Crunchyroll video downloader')
+    parser.add_argument(
+        dest='url', nargs='?', action='store', metavar='URL',
+        help='Video url from Crunchyroll to download')
+    parser.add_argument(
+        '-l', '--login', action='store_true', default=0,
+        help='Login to your Crunchyroll account')
     version_info = '%(prog)s {}'.format(__version__)
-    parser.add_argument('-v', '--version', action='version',
-    		version=version_info)
+    parser.add_argument(
+        '-v', '--version', action='version',
+        version=version_info)
     return parser
+
 
 def yes_no_query(question):
     print('{} (y/n)'.format(question))
@@ -28,13 +33,14 @@ def yes_no_query(question):
         except ValueError:
             print("Please respond with 'y' or 'n'.")
 
+
 def main():
-	#If no option is given, show help and exit
+    # If no option is given, show help and exit
     parser = argparser()
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(2)
-    #Parse the user choices
+    # Parse the user choices
     args = parser.parse_args()
 
     if args.login:
@@ -52,7 +58,7 @@ def main():
             print 'Cookies created successfuly!'
 
     if args.url:
-    	downloader.getVideo(args.url)
+        downloader.getVideo(args.url)
 
 if __name__ == '__main__':
-	main()
+    main()
