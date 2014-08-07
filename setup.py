0242c131126af252352b44db3943db2541529ee6
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-08-07 20:46:32 -0300

    Remove support for Python 2.6
    Only because Python 2.6 is very old and doesn't have argparse. It should
    still work, but now it's problem of the user.

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         "Environment :: Console",
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Multimedia :: Sound/Audio",
